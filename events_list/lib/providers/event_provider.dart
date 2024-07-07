81dccd90aa16b14f3768d33f07b9b446d1d777b5
tef <teferi.aleme@gmail.com>
2024-07-07 00:30:08 -0400

    git init

---
+++ events_list/lib/providers/event_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../models/event.dart';
+import '../services/api_service.dart';
+import '../services/cache_service.dart';
+import '../services/location_service.dart';
+
+class EventProvider with ChangeNotifier {
+  List<Event> _events = [];
+  bool _loading = false;
+  final ApiService _apiService = ApiService();
+  final CacheService _cacheService = CacheService();
+  final LocationService _locationService = LocationService();
+
+  List<Event> get events => _events;
+  bool get loading => _loading;
+
+  Future<void> fetchEvents({String? city}) async {
+    _loading = true;
+    notifyListeners();
+
+    try {
+      city ??= await _locationService.getCityName();
+      final cachedEvents = await _cacheService.getCachedEvents(city);
+
+      if (cachedEvents != null) {
+        _events = cachedEvents.map((e) => Event.fromJson(e)).toList();
+      } else {
+        final response = await _apiService.getEvents(city);
+        _events = response.map((e) => Event.fromJson(e)).toList();
+        await _cacheService.cacheEvents(city, response);
+      }
+    } catch (error) {
+      print("fetchEvents: city: $city error:$error");
+      _events = [];
+    }
+
+    _loading = false;
+    notifyListeners();
+  }
+}
