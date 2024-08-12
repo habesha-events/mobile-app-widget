817dadd64b8f836310b55ef6b2203444bfb04155
tef <teferi.aleme@gmail.com>
2024-08-12 13:59:01 -0700

    onErrorCallback

--- events_list/lib/main.dart
+++ events_list/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        home: EventListWidget(),
+        home: EventListWidget(onErrorCallback: (){},),
         debugShowCheckedModeBanner: false,
       ),
     );
