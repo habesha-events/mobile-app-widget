3a6e8b053d9d5d8042a743b5a541d6fc4156e7af
tef <teferi.aleme@gmail.com>
2024-09-22 13:09:33 -0700

    fix location issue

--- events_list/lib/services/location_service.dart
+++ events_list/lib/services/location_service.dart
@@ -6,47 +6,55 @@ String defaultCity = 'Seattle';
 
 class LocationService {
   Future<String> getCityName() async {
-    final loc.Location location = loc.Location();
-    bool _serviceEnabled;
-    loc.PermissionStatus _permissionGranted;
-    loc.LocationData _locationData;
-
-    print("LocationService: getCityName: 1");
-
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        // return Future.error('Location services are disabled.');
-        return defaultCity;
-      }
-    }
-
-    print("LocationService: getCityName: 2: _serviceEnabled ${_serviceEnabled}");
-
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == loc.PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != loc.PermissionStatus.granted) {
-        // return Future.error('Location permissions are denied');
-        return defaultCity;
-      }
-    }
-    print("LocationService: getCityName: 3: _permissionGranted ${_permissionGranted}");
-
-    _locationData = await location.getLocation();
-    print("LocationService: getCityName: 4: latitude ${_locationData.latitude}, longitude ${ _locationData.longitude}");
-
     var city = defaultCity;
-    try{
-      List<Placemark> placemarks = await placemarkFromCoordinates(
-          _locationData.latitude!, _locationData.longitude!);
-      city =  placemarks.first.locality ?? 'Unknown';
-    }catch(e){
-      print("LocationService: PlaceMark Error: ${e}");
-    }
 
-    print("LocationService: getCityName: 5: city ${city}");
+    try {
+      final loc.Location location = loc.Location();
+      bool _serviceEnabled;
+      loc.PermissionStatus _permissionGranted;
+      loc.LocationData _locationData;
+
+      print("LocationService: getCityName: 1");
+
+      _serviceEnabled = await location.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await location.requestService();
+        if (!_serviceEnabled) {
+          // return Future.error('Location services are disabled.');
+          return defaultCity;
+        }
+      }
+
+      print(
+          "LocationService: getCityName: 2: _serviceEnabled ${_serviceEnabled}");
+
+      _permissionGranted = await location.hasPermission();
+      if (_permissionGranted == loc.PermissionStatus.denied) {
+        _permissionGranted = await location.requestPermission();
+        if (_permissionGranted != loc.PermissionStatus.granted) {
+          // return Future.error('Location permissions are denied');
+          return defaultCity;
+        }
+      }
+      print(
+          "LocationService: getCityName: 3: _permissionGranted ${_permissionGranted}");
+
+      _locationData = await location.getLocation();
+      print("LocationService: getCityName: 4: latitude ${_locationData
+          .latitude}, longitude ${ _locationData.longitude}");
+
+      try {
+        List<Placemark> placemarks = await placemarkFromCoordinates(
+            _locationData.latitude!, _locationData.longitude!);
+        city = placemarks.first.locality ?? 'Unknown';
+      } catch (e) {
+        print("LocationService: PlaceMark Error: ${e}");
+      }
+
+      print("LocationService: getCityName: 5: city ${city}");
+    } catch (e) {
+      print("LocationService: General Error: ${e}");
+    }
 
     return city;
   }
