76db5e21db495e71680db1711e995354f9acd3fc
tef <teferi.aleme@gmail.com>
2024-10-12 08:08:48 -0700

    fix

--- events_list/lib/services/location_service.dart
+++ events_list/lib/services/location_service.dart
@@ -3,7 +3,7 @@ import 'package:geocoding/geocoding.dart';
 
 List<String> SUPPORTED_CITIES = ['Washington, DC',
   'Addis Ababa',
-  'Nairobi', 'Dubai'
+  'Nairobi', 'Dubai',
   'Johannesburg', 'Minneapolis', 'Los Angeles', 'New York City', 'Seattle', 'Dallas', 'Atlanta', 'Denver', 'San Francisco', 'Boston', 'Houston', 'Chicago', 'San Diego', 'Philadelphia', 'Phoenix', 'Portland', 'Austin', 'Miami', 'Detroit', 'Baltimore', 'Toronto', 'Calgary', 'Edmonton', 'Vancouver', 'Montreal', 'Ottawa', 'Winnipeg', 'Hamilton', 'Kitchener', 'London', 'Halifax', 'Victoria', 'Quebec City', 'Surrey', 'Mississauga', 'Burnaby', 'Regina', 'Saskatoon', 'Windsor', 'Oshawa', 'London', 'Frankfurt', 'Stockholm', 'Rome', 'Amsterdam', 'Paris', 'Berlin', 'Oslo', 'Brussels', 'Copenhagen', 'Madrid', 'Vienna', 'Zurich', 'Munich', 'Lisbon', 'Helsinki', 'Dublin', 'Athens', 'Prague', 'Warsaw'];
 String defaultCity = 'Seattle';
 
